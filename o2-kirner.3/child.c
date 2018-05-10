78960e58c12bbe8ba075a99fac2f78f8f2659537
jkirner <33811924+jkirner@users.noreply.github.com>
2018-05-10 03:46:36 -0500

    Update child.c

--- o2-kirner.3/child.c
+++ o2-kirner.3/child.c
@@ -30,46 +30,58 @@ typedef struct{
   info myinfo;
 }mymsg;
 
-int main () {
+int main (int argc, char *argv[]) {
   info myinfo;
   int id;
-  
+  int msgqid;
+  int msgqid2;
+  mymsg dummyMes;
   
   if((id = shmget(411, sizeof(clockS), IPC_CREAT|0666)) == -1) {
     perror(("%s: Error: Failed to attached shared memory segment", argv[0]));
     return 1;
   }
-  if((clockS *sharedClock= (clockS *)shmat(id, NULL, 0)) == (void *)-1){
+  clockS *sharedClock= (clockS *)shmat(id, NULL, 0);
+  if(sharedClock == (void *)-1){
     perror(("%s: Error: Failed to attach shared memory segment", argv[0]));
     if(shmctl(id, IPC_RMID, NULL) == -1)
-      perror((%s: Error: Failed to remove memory segment", argv[0]));
+      perror(("%s: Error: Failed to remove memory segment", argv[0]));
     return 1;
   }
   myinfo.childPid = getpid();
   msgqid = msgget (412, IPC_CREAT | 0777);
   msgqid2 = msgget (413, IPC_CREAT | 0777);
   
-  if ((msgrcv (msgqid, &dummyMes, sizeof(dummyMes), 0)) == -1) {
+  if ((msgrcv (msgqid, &dummyMes, sizeof(dummyMes), 0, 0)) == -1) {
       perror(("%s: Error: Failed to recieve message"));
-	  if (detachandremove(id, sharedClock) == -1)
-        perror(("%s: Error: Failed to destory shared memory segment"));
+	  if (shmdt (sharedClock)) {
+        perror (("%s: Error: Failed to detach shared memory", argv[0]));
+        exit(EXIT_FAILURE);
+      }
+      if ((shmctl (id, IPC_RMID, NULL)) == -1) {
+        perror (("%s: Error: Failed to destroy shared memory", argv[0]));
+        exit(EXIT_FAILURE);
+      }
 	  return 1;
     }
   printf("%d %d %d", myinfo.childPid, sharedClock.sec, sharedClock.nano);
   if ((msgrcv (msgqid2, &dummyMes, sizeof(dummyMes), 0)) == -1) {
       perror(("%s: Error: Failed to recieve message"));
-	  if (detachandremove(id, sharedClock) == -1)
-        perror(("%s: Error: Failed to destory shared memory segment"));
+	  if (shmdt (sharedClock)) {
+        perror (("%s: Error: Failed to detach shared memory", argv[0]));
+        exit(EXIT_FAILURE);
+      }
+      if ((shmctl (id, IPC_RMID, NULL)) == -1) {
+        perror (("%s: Error: Failed to destroy shared memory", argv[0]));
+        exit(EXIT_FAILURE);
+      }
 	  return 1;
     }
-  int stuffToDo = rand(childId) % 100000 + 1;
+  int stuffToDo = 50000 + 1;
   sharedClock.nano += stuffToDo;
-  mymsg dummyMes;
+  
   dummyMes.myinfo = myinfo;
   msgsnd (msgqid, &dummyMes, sizeof(dummyMes),  0);
 // wait for msg
-
-  
-}
   return 0;
 }
